7b7e787ea9286ddd6e48c3b2455f43253695c3ff
Aleksei Tokarev <atokarev@hylamobile.com>
2019-09-11 22:10:32 -0500

    ItemDto.kt

---
+++ src/main/kotlin/com/hylamobile/v2/ItemDto.kt
@@ -0,0 +1,10 @@
+package com.hylamobile.v2
+
+data class ItemDto(
+    val itemId: Long,
+    val modelCode: String,
+    val esn: String,
+    var quantity: Int
+) {
+    val invoiceId: Long = 0
+}
